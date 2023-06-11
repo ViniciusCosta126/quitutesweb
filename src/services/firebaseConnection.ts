150531e61e12fb19c183931e8e19d63d42a57320
viniciuscosta126 <viniciuscosta126@gmail.com>
2023-06-11 18:24:13 -0300

    Configuracao inicial firebase

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,12 @@
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9YxvBU2FEobmfyLkiYkSOZgz8FiRM8d8",
+    authDomain: "quitutes-fb1c3.firebaseapp.com",
+    projectId: "quitutes-fb1c3",
+    storageBucket: "quitutes-fb1c3.appspot.com",
+    messagingSenderId: "1052656770595",
+    appId: "1:1052656770595:web:ea0d55ab2485c706a9db96"
+}
+
+export const app = initializeApp(firebaseConfig)
